a3a87a6a08f1dc1a740d385c1aa2e0376f5b98d5
thiensubs <nguoitinh.cuaanh.12@gmail.com>
2014-05-17 16:09:39 +0700

    New function and config
    New function
    Config
    Fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515065312) do
+ActiveRecord::Schema.define(version: 20140517085147) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -72,8 +72,13 @@ ActiveRecord::Schema.define(version: 20140515065312) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
