a51fefee65238ff65c477da8568ebad09dfd0822
thiensubs <nguoitinh.cuaanh.12@gmail.com>
2014-05-18 16:56:50 +0700

    Change mail send.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'test_nus@123.com - Mailer System'
+  config.mailer_sender = 'test_nus@123.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
