da1a71693dd203e842329d560ef34c53faecabc2
An Vo <nguoitinh.cuaanh.12@gmail.com>
2014-05-20 13:40:46 +0700

    Fix

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module TestNusProject
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += Ckeditor.assets
+
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
     def default_url_options(options={})
