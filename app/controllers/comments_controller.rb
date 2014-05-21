d0a60f6ca7e9bb1c65482d1ebe76d4faeca0aabf
An Vo <nguoitinh.cuaanh.12@gmail.com>
2014-05-21 17:55:45 +0700

    Fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,7 +33,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @comment, notice: t("comment_updated") }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -48,7 +48,7 @@ class CommentsController < ApplicationController
     @post = Post.friendly.find(params[:post_id])
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to post_path(@post), notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to post_path(@post), notice:  t("comment_destroy")  }
       format.json { head :no_content }
     end
   end
