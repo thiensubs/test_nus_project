3d4c8dd70ef9b6c844db5aefc9fccf99732dbfcd
An Vo <nguoitinh.cuaanh.12@gmail.com>
2014-05-21 17:46:29 +0700

    Remove un-used code for initialize cancan.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
       user ||= User.new # guest user (not logged in)
-      if user
-        can :manage, [Post, Comment], :user_id => user.id
-
-      else
-        can :read, :all
-      end
+      can :manage, [Post, Comment], :user_id => user.id
 
     # The first argument to `can` is the action you are giving the user
     # permission to do.
