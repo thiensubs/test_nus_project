d44057fd4c621ffa285309650cc916db2855e8bf
An Vo <nguoitinh.cuaanh.12@gmail.com>
2014-05-22 14:01:26 +0700

    Fix i18n

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, notice: t("post_success_msg") }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -50,7 +50,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, notice: t("post_update_msg") }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -64,7 +64,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: t("post_destroy_msg") }
       format.json { head :no_content }
     end
   end
